8948c2a4ff2eacfa778077f466f97813b5ed8948
usman <usmankhan@uo.edu.pk>
2024-10-13 18:00:14 +0200

    Update UserLevel.cs

--- Application/UserLevel.cs
+++ Application/UserLevel.cs
@@ -1,19 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Application
+﻿namespace Application
 {
     public enum UserLevel
     {
         ADMIN = 1,
         OPERATION_MANAGER = 2,
-        SITE_MANAGER =3,
+        SITE_MANAGER = 3,
         REGISTER_MANAGER = 4,
-        KEY_WORKER =5,
+        KEY_WORKER = 5,
         SOCIAL_WORKER = 6,
-        DIRECTOR=7
-}
+        DIRECTOR = 7
+    }
 }
